389736ed8c9c262b4309f08bc61c1b36558517ef
Cesar SC <cesar050782sc@gmail.com>
2017-01-15 23:05:08 +0100

    Add files via upload
    GOLANG interacting with NODEMCU

---
+++ GOLANG.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"bytes"
+	"net"
+	"bufio"
+)
+
+func main()  {
+	
+	http.HandleFunc("/nodemcu", func(res http.ResponseWriter, req *http.Request) {
+		conn, err := net.Dial("tcp", "192.168.1.39:80")
+		if err != nil {
+			panic(err)
+		}
+		defer conn.Close()
+
+		conn.Write([]byte("11"))
+		resp, _, _ := bufio.NewReader(conn).ReadLine()
+		fmt.Println(string(resp))
+	})
+	
+	http.ListenAndServe(":8080", nil)
+
+}
